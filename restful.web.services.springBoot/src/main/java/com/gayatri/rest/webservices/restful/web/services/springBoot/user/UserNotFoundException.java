05b770f8b3ac63078963248ef02e4cdc024c239c
Gayatrigurav <gayatri.gurav86@gmail.com>
2020-05-26 01:29:17 -0500

    RESTful webservices first commit

---
+++ restful.web.services.springBoot/src/main/java/com/gayatri/rest/webservices/restful/web/services/springBoot/user/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package com.gayatri.rest.webservices.restful.web.services.springBoot.user;
+
+public class UserNotFoundException extends RuntimeException {
+
+	public UserNotFoundException(String message) {
+		super(message);
+		
+	}
+
+	
+	
+}
