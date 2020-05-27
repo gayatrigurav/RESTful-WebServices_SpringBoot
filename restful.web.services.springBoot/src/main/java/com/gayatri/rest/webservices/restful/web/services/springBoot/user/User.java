c2103b36802aa1030918144921fe26e9562d0eb5
Gayatrigurav <gayatri.gurav86@gmail.com>
2020-05-27 01:59:19 -0500

    Monitoring APIs with Spring Boot Actuator

--- restful.web.services.springBoot/src/main/java/com/gayatri/rest/webservices/restful/web/services/springBoot/user/User.java
+++ restful.web.services.springBoot/src/main/java/com/gayatri/rest/webservices/restful/web/services/springBoot/user/User.java
@@ -5,14 +5,20 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+//@ApiModel(description="All details about the user")
 public class User {
 	
 	private Integer id;
 	
 	@Size(min=2, message="Name should have atleast 2 charecters")
+	//@ApiModelProperty(notes="Name should have atleast 2 charecters")
 	private String name;
 	
 	@Past
+	//@ApiModelProperty(notes="Birth date should not be in the past")
 	private Date birthDate;
 	
 	protected User() {
